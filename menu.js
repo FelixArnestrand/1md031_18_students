e812c879a80c10b554c77ffec6faaf70668c60fc
FelixArnestrand <57362159+FelixArnestrand@users.noreply.github.com>
2019-12-02 16:07:42 +0100

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,39 @@
+var food = [
+	{
+	"name": "The special one",
+	"kCal": 400,
+	"lactose": true,
+	"gluten": true,    
+	"img": "img/Burger1.jpg"
+	},
+		{
+	"name": "The chicken one",
+	"kCal": 350,
+	"lactose": false,
+	"gluten": true,    
+	"img": "img/Burger2.png"
+	},
+		{
+	"name": "The skinny one",
+	"kCal": 200,
+	"lactose": false,
+	"gluten": false,    
+	"img": "img/Burger3.jpg"
+	},
+		{
+	"name": "The fat one",
+	"kCal": 800,
+	"lactose": true,
+	"gluten": true,    
+	"img": "img/theFatOne.jpg"
+	},
+		{
+	"name": "The green one",
+	"kCal": 400,
+	"lactose": true,
+	"gluten": false,    
+	"img": "img/theGreenOne.jpg"
+	},
+	
+
+]
